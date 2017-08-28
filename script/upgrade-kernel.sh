efc2f3c1d8438aa129e71c2c62ba469904cf6cb2
Ian Li <techotaku@users.noreply.github.com>
2017-08-28 23:37:27 +0800

    Use kernel 4.9.45

--- script/upgrade-kernel.sh
+++ script/upgrade-kernel.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 uname -r
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.43/linux-headers-4.9.43-040943_4.9.43-040943.201708122332_all.deb
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.43/linux-headers-4.9.43-040943-generic_4.9.43-040943.201708122332_amd64.deb
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.43/linux-image-4.9.43-040943-generic_4.9.43-040943.201708122332_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.45/linux-headers-4.9.45-040945_4.9.45-040945.201708242131_all.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.45/linux-headers-4.9.45-040945-generic_4.9.45-040945.201708242131_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.45/linux-image-4.9.45-040945-generic_4.9.45-040945.201708242131_amd64.deb
 sudo dpkg -i linux-*.deb
 rm linux-*.deb
