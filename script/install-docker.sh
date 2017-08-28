cd699be4ffc8d83a1ec33a56a78927ec95d5f49b
Ian Li <techotaku@users.noreply.github.com>
2017-08-28 23:42:03 +0800

    Enable live-restore by default.

--- script/install-docker.sh
+++ script/install-docker.sh
@@ -7,3 +7,6 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get install -y docker-ce
 sudo systemctl enable docker
+
+sudo mkdir -p /etc/docker
+echo "{ \"live-restore\": true }" | sudo tee /etc/docker/daemon.json
