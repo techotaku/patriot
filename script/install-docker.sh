5228e21d6b85b3f306534df6a28c84f481c12e68
Ian Li <techotaku@users.noreply.github.com>
2017-12-15 01:30:23 +0800

    Update compose.yml

--- script/install-docker.sh
+++ script/install-docker.sh
@@ -6,7 +6,6 @@ sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
-sudo systemctl enable docker
 
 sudo mkdir -p /etc/docker
 echo "{ \"live-restore\": true }" | sudo tee /etc/docker/daemon.json
--- script/upgrade-kernel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-uname -r
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.45/linux-headers-4.9.45-040945_4.9.45-040945.201708242131_all.deb
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.45/linux-headers-4.9.45-040945-generic_4.9.45-040945.201708242131_amd64.deb
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.45/linux-image-4.9.45-040945-generic_4.9.45-040945.201708242131_amd64.deb
-sudo dpkg -i linux-*.deb
-rm linux-*.deb
